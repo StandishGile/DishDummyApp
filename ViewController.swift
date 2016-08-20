c3aeb6bbf51c07c8838f6c57ce37c0b7ed8f7d4c
Dish Xcode <standishgile@yahoo.com>
2016-08-20 12:56:12 -0700

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dish Dummy App
+//
+//  Created by Dish Xcode on 8/20/16.
+//  Copyright © 2016 Dish Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
